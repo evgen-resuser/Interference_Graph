b59babf7b9f47fb773804f16c35a27e21ef2aa4e
Эвген <108253811+evgen-resuser@users.noreply.github.com>
2023-06-17 23:46:14 +0700

    alpha

---
+++ Picture.java
@@ -0,0 +1,53 @@
+import javax.swing.*;
+import java.awt.*;
+import java.util.List;
+
+public class Picture extends JPanel {
+
+    private List<Double> array;
+    private IntensityGraph graph;
+
+
+    public Picture(IntensityGraph graph){
+        this.setVisible(true);
+        this.setSize(400, 50);
+
+        array = graph.getArray();
+        this.graph = graph;
+    }
+
+    void drawLines(Graphics g){
+        Graphics2D g2d = (Graphics2D) g;
+
+        int x1 = 0;
+        int y1 = 0;
+        int x2 = 0;
+        int y2 = 50;
+
+        BasicStroke stroke = new BasicStroke(3f);
+
+        int indx = 0;
+        double max = graph.getMaxI();
+
+        for(double i = -1.5708; i < 1.5708; i+=0.01){
+            g2d.drawLine(x1, y1, x2, y2);
+            g2d.setStroke(stroke);
+
+            int br = (int)(array.get(indx)/max*255);
+
+            Color color = new Color(br, br, br);
+            g2d.setColor(color);
+
+            x1+=2;
+            x2+=2;
+            indx++;
+        }
+    }
+
+    @Override
+    public void paint(Graphics g){
+        super.paint(g);
+        drawLines(g);
+    }
+
+}
